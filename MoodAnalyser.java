d1b588bcee8cb4dcdb8efebc3a78c7d86319d3e8
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-06 19:17:20 +0530

    MaheswariKoyyani : Happy and Sad mood Using Constructors

--- MoodAnalyser.java
+++ MoodAnalyser.java
@@ -1,12 +1,23 @@
 package com.capgemini.moodAnalyser1;
 
 public class MoodAnalyser {
+	private String message;
 
-public String analyseMood(String message) {
+	public MoodAnalyser(String message) {
+		this.message=message;
+	}
+	public MoodAnalyser() {
+		// TODO Auto-generated constructor stub
+	}
+
+public String analyseMood() {
 	if(message.contains(("Sad"))) {
 		return "SAD";
 	}
 	else
 		return "HAPPY";
+}
+public static void main(String[] args) {
+	
 }
 }
--- MoodAnalyserTest.java
+++ MoodAnalyserTest.java
@@ -6,14 +6,14 @@ import org.junit.Test;
 public class MoodAnalyserTest {
 	@Test
 	public void givenMessage_ShouldReturn_Sad() {
-		MoodAnalyser moodAnalyser =new MoodAnalyser();
-		String mood =moodAnalyser.analyseMood("I am in Sad mood");
+		MoodAnalyser moodAnalyser =new MoodAnalyser("I am in Sad mood");
+		String mood =moodAnalyser.analyseMood();
 		Assert.assertEquals("SAD",mood);
 	}
 	@Test
 	public void givenMessage_ShouldReturn_Happy() {
-		MoodAnalyser moodAnalyser =new MoodAnalyser();
-		String mood =moodAnalyser.analyseMood("I am in any mood");
+		MoodAnalyser moodAnalyser =new MoodAnalyser("I am in any mood");
+		String mood =moodAnalyser.analyseMood();
 		Assert.assertNotEquals("Happy",mood);
 	}
 
